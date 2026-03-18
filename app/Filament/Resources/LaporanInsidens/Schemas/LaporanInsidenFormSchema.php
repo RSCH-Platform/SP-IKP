bddbf6bfe0e97414336899ccd24dcdbb57e59b9c
juniyasyos <juniyasyos@gmail.com>
2026-03-18 15:47:04 +0700

    feat: Remove debug statements from getFieldTabularTimeline method in LaporanInsidenFormSchema

--- app/Filament/Resources/LaporanInsidens/Schemas/LaporanInsidenFormSchema.php
+++ app/Filament/Resources/LaporanInsidens/Schemas/LaporanInsidenFormSchema.php
@@ -836,11 +836,6 @@ class LaporanInsidenFormSchema
                                 $merged = $categories->map(function ($category) use ($existing) {
                                     $found = $existing->firstWhere('category_id', $category->id);
 
-                                    dd([
-                                        'category_id' => $category->id,
-                                        'category_name' => $category->name,
-                                        'description' => $found['description'] ?? null,
-                                    ]);
                                     return $found
                                         ? array_merge($found, [
                                             'category_name' => $category->name,
