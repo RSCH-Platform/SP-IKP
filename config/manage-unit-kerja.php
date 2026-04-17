71cafc82f0debbd3c7b93f939990194830c1e3e7
juniyasyos <juniyasyos@gmail.com>
2026-04-17 16:27:34 +0700

    fix: Update app_center_url to use IAM_HOST for correct environment configuration

--- config/manage-unit-kerja.php
+++ config/manage-unit-kerja.php
@@ -20,7 +20,7 @@ return [
 
     'center_application' => env('MANAGE_UNIT_KERJA_CENTER_APPLICATION', false),
 
-    'app_center_url' => env('MANAGE_UNIT_KERJA_APP_CENTER_URL', 'http://127.0.0.1:8010'),               
+    'app_center_url' => env('IAM_HOST', 'http://192.168.1.9:8100'),               
 
     'sync' => [
         'active' => env('MANAGE_UNIT_KERJA_SYNC_ACTIVE', true),
@@ -28,3 +28,4 @@ return [
 
     'navigation_sort' => 0,
 ];
+k
