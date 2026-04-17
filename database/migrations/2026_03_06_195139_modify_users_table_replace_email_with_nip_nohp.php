d4eec94db93ab508bdd787d8a5cda56442dd1b2a
juniyasyos <juniyasyos@gmail.com>
2026-04-18 01:48:54 +0700

    fix: Update users table to replace email with nip and no_hp fields

--- database/migrations/2026_03_06_195139_modify_users_table_replace_email_with_nip_nohp.php
+++ database/migrations/2026_03_06_195139_modify_users_table_replace_email_with_nip_nohp.php
@@ -9,7 +9,6 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->dropColumn(['email', 'email_verified_at']);
             $table->string('nip')->nullable()->unique()->after('name');
             $table->string('no_hp', 12)->unique()->after('nip');
         });
@@ -19,8 +18,6 @@ return new class extends Migration
     {
         Schema::table('users', function (Blueprint $table) {
             $table->dropColumn(['nip', 'no_hp']);
-            $table->string('email')->unique()->after('name');
-            $table->timestamp('email_verified_at')->nullable()->after('email');
         });
     }
 };
